387c5ae02eab6ecc7ed09cbfc8669c6f10adda46
HABAfOROMO <fhaba639@gmail.com>
2026-09-08 09:14:52 +0000

    Ajout du script de build pour Vercel (installation de Flutter)

---
+++ vercel_build.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+# Script de build pour Vercel : Flutter n est pas preinstalle, donc on
+# le telecharge nous-memes avant de compiler la version web de l app.
+set -e
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+flutter doctor
+flutter pub get
+flutter build web --dart-define=API_BASE_URL=https://cnfpatel-backend.onrender.com/api
